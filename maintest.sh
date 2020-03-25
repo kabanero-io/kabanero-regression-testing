fcb155d2d610f77f45ec05f9f037627b513b905b
Thomas E. Link <tomlink@us.ibm.com>
2020-03-25 10:38:35 -0400

    Script Updates to Handle errors

--- maintest.sh
+++ maintest.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-# *****************************************************************
-#
-# Licensed Materials - Property of IBM
-#
-# (C) Copyright IBM Corp. 2020. All Rights Reserved.
-#
-# US Government Users Restricted Rights - Use, duplication or
-# disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
-#
-# *****************************************************************
 
 MAIN_DIR=`dirname $0`
 cd "${MAIN_DIR}"
@@ -22,17 +12,34 @@ fi
 # Time to run tests now
 cd ${MAIN_DIR}/tests
 
+let anyfail=0
+
 # loop on all directories
 for testcase in `ls -d *`; do
-   echo "Running testcase $testcase"
+   echo "*** Running testcase $testcase"
    cd $testcase
    if [ -f test.sh ]; then
      ./test.sh
+     if [ $? -ne 0 ]; then
+       let anyfail+=1
+     fi
      # do something to publish test results
    elif [ -f test.yaml ]; then
      ansible-playbook test.yaml
+     if [ $? -ne 0 ]; then
+       let anyfail+=1
+     fi
      # do something to publish test results
    else
-     echo "No test found in $testcase"
+     echo "*** No test found in $testcase"
    fi
 done
+
+# Summarize results
+if [ $anyfail -eq 0 ]; then
+   echo "*** All tests ran without error"
+else
+   echo "*** There were $anyfail test failures"
+fi 
+
+exit $anyfail
