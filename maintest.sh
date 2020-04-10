19fe241b9540c1fd72beaf1217fcf31c9adc96a4
Steven Schader <schader@us.ibm.com>
2020-04-10 11:48:51 -0400

    streamlined locating tests

--- maintest.sh
+++ maintest.sh
@@ -1,47 +1,44 @@
 #!/bin/bash
 
-MAIN_DIR=`dirname $0`
-cd "${MAIN_DIR}"
-MAIN_DIR=`pwd`
-
 if ! oc whoami; then
     echo "Must login using oc before running"
     exit 1
 fi
 
 # Time to run tests now
-cd ${MAIN_DIR}/tests
+cd $(dirname $(readlink -f $0))/tests
 
 let anyfail=0
 failed=""
 
-# loop on all directories
-for testcase in `ls -d *`; do
-   if [ -d "$testcase" ] ; then
+# find any .sh|test.yaml|test.yml
+regressionTestScripts=$(find . -name [0-9]* -type f |egrep '*.sh|*.yml|*.yaml'| sort)
+for testcase in $( echo "$regressionTestScripts") ; do
+   if [ -f "$testcase" ] ; then
      echo "*** Running testcase $testcase"
-     cd $testcase
-     if [ -f test.sh ]; then
-       ./test.sh
+     cd $(dirname "$testcase")
+     if [[ $testcase == *.sh ]] ; then
+       ./$(basename "$testcase")
        if [ $? -ne 0 ]; then
          let anyfail+=1
          failed="$failed $testcase"
        fi
-       # do something to publish test results
-     elif [ -f test.yaml ]; then
-       ansible-playbook test.yaml
-       if [ $? -ne 0 ]; then
-         let anyfail+=1
-         failed="$failed $testcase"
-       fi
-       # do something to publish test results
-     else
-       echo "*** No test found in $testcase"
      fi
+     if [[ $testcase == *.yaml ]] || [[ $testcase == *.yml ]] ; then
+       ansible-playbook $(basename "$testcase")
+       if [ $? -ne 0 ]; then
+         let anyfail+=1
+         failed="$failed $testcase"
+       fi
+     fi
+     cd -
+   else
+     echo "*** No test found in $testcase"
    fi
 done
 
 # Summarize results
-if [ $anyfail -eq 0 ]; then
+if [ $anyfail -eq 0 ] ; then
    echo "*** All testcases ran without error"
 else
    echo "*** There were $anyfail testcase failures - $failed"
